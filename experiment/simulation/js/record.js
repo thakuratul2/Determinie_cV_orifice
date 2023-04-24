4eb4237071c1b49c94f2f86bacca0e996f41ab3b
thakuratul2 <pratapsinghatul111@gmail.com>
2023-04-24 19:10:54 +0530

    Simulation 1

--- experiment/simulation/js/record.js
+++ experiment/simulation/js/record.js
@@ -2,27 +2,58 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+document.getElementById("read1").style.visibility = "hidden";
+
+var output1 = document.getElementById("range-value");
+output1.innerHTML=19;
+slider.oninput = function()
+
+{
+  if(slider.value==63.1){
+    output.innerHTML = this.value;
+  output1.innerHTML=19;
+  
 }
-
-
-
+else if(slider.value==65.3){
+  output.innerHTML = 65.1;
+    output1.innerHTML = 18.5;
+   
+}
+else if(slider.value==67.5){
+  output.innerHTML = 67.9;
+  output1.innerHTML=18;
  
+}
+else if(slider.value==69.7){
+  output.innerHTML = 70;
+  output1.innerHTML=17;
+  
+}
+else{
+  output1.innerHTML=0;
+}}
+ 
+
+
+
+
 function datasheet() {
-  document.getElementById("sno").innerHTML = "1";
+  if(slider.value==69.7)
+  {
+    document.getElementById("sno").innerHTML = "1";
   document.getElementById("height").innerHTML = "75.015";
   document.getElementById("x").innerHTML = "70";
   document.getElementById("y").innerHTML = "17";
-  document.getElementById("t").innerHTML = "10";
+  document.getElementById("t").innerHTML = "146";
 
   var x=70;
   var y=17;
   
-  var t=10;
-  var area=30;
+  var t=146;
+  var area=2500;
   var r=20;
   var qa= (area*r)/t;
+  var qa=parseFloat(qa).toFixed( 3 );
   document.getElementById("qa").innerHTML = qa;
 
   var areaor=1.2667;
@@ -30,150 +61,156 @@ function datasheet() {
   var c= areaor* Math.sqrt(19.62);          // 2*g= 19.62
   var H=75.015;                          // H=head
   var qth= c * Math.sqrt(H);              
-  var qth=parseFloat(qth).toFixed( 4 );
+  var qth=parseFloat(qth).toFixed( 3 );
   document.getElementById("qth").innerHTML = qth;
 
   
   var cd=qa/qth;
-  var cd=parseFloat(cd).toFixed( 4 );
+  var cd=parseFloat(cd).toFixed( 3 );
   document.getElementById("cd").innerHTML = cd;
   
   var de=4*y*H;
   var cv= x/Math.sqrt(de);
-  var cv=parseFloat(cv).toFixed( 4 );
-  document.getElementById("cv").innerHTML = cv;
+  var cv=parseFloat(cv).toFixed( 3 );
+  //document.getElementById("cv").innerHTML = cv;
 
   var cc=cd/cv;
-  var cc=parseFloat(cc).toFixed( 4 );
-  document.getElementById("cc").innerHTML = cc;
+  var cc=parseFloat(cc).toFixed( 3 );
+ // document.getElementById("cc").innerHTML = cc;
+ document.getElementById("read1").style.visibility = "visible";
+}
 
-
-  document.getElementById("sno1").innerHTML = "2";
-  document.getElementById("height1").innerHTML = "67.915";
-  document.getElementById("x1").innerHTML = "67.9";
-  document.getElementById("y1").innerHTML = "18";
-  document.getElementById("t1").innerHTML = "10";
+else if(slider.value==67.5)
+  {
+    document.getElementById("sno").innerHTML = "1";
+  document.getElementById("height").innerHTML = "67.915";
+  document.getElementById("x").innerHTML = "67.9";
+  document.getElementById("y").innerHTML = "18";
+  document.getElementById("t").innerHTML = "151";
 
   var x1=67.9;
   var y1=18;
   
-  var t=10;
-  var area=30;
+  var t=151;
+  var area=2500;
   var r=20;
   var qa= (area*r)/t;
-  document.getElementById("qa1").innerHTML = qa;
+  var qa=parseFloat(qa).toFixed( 3 );
+  document.getElementById("qa").innerHTML = qa;
 
   var areaor=1.2667;
   var g=9.81;
   var c= areaor* Math.sqrt(19.62);
   var H=67.915;                          // H=head
   var qth= c * Math.sqrt(H); 
-  var qth=parseFloat(qth).toFixed( 4 );
-  document.getElementById("qth1").innerHTML = qth;
+  var qth=parseFloat(qth).toFixed( 3 );
+  document.getElementById("qth").innerHTML = qth;
 
   
   var cd=qa/qth;
-  var cd=parseFloat(cd).toFixed( 4 );
-  document.getElementById("cd1").innerHTML = cd;
+  var cd=parseFloat(cd).toFixed( 3 );
+  document.getElementById("cd").innerHTML = cd;
   
   var de=4*y1*H;
   var cv= x1/Math.sqrt(de);
-  var cv=parseFloat(cv).toFixed( 4 );
-  document.getElementById("cv1").innerHTML = cv;
+  var cv=parseFloat(cv).toFixed( 3 );
+  //document.getElementById("cv1").innerHTML = cv;
 
   var cc=cd/cv;
-  var cc=parseFloat(cc).toFixed( 4 );
-  document.getElementById("cc1").innerHTML = cc;
+  var cc=parseFloat(cc).toFixed( 3 );
+  //document.getElementById("cc1").innerHTML = cc;
+  document.getElementById("read1").style.visibility = "visible";
+}
 
 
+  else if(slider.value==65.3)
 
-
-  document.getElementById("sno2").innerHTML = "3";
-  document.getElementById("height2").innerHTML = "51.765";
-  document.getElementById("x2").innerHTML = "65.1";
-  document.getElementById("y2").innerHTML = "18.5";
-  document.getElementById("t2").innerHTML = "10";
+  {
+    document.getElementById("sno").innerHTML = "1";
+  document.getElementById("height").innerHTML = "51.765";
+  document.getElementById("x").innerHTML = "65.1";
+  document.getElementById("y").innerHTML = "18.5";
+  document.getElementById("t").innerHTML = "179";
 
   var x2=65.1;
   var y2=18.5;
   
-  var t=10;
-  var area=30;
+  var t=179;
+  var area=2500;
   var r=20;
   var qa= (area*r)/t;
-  document.getElementById("qa2").innerHTML = qa;
+  var qa=parseFloat(qa).toFixed( 3 );
+  document.getElementById("qa").innerHTML = qa;
 
   var areaor=1.2667;
   var g=9.81;
   var c= areaor* Math.sqrt(19.62);
   var H=51.765;                          
   var qth= c * Math.sqrt(H); 
-  var qth=parseFloat(qth).toFixed( 4 );
-  document.getElementById("qth2").innerHTML = qth;
+  var qth=parseFloat(qth).toFixed( 3 );
+  document.getElementById("qth").innerHTML = qth;
 
   
   var cd=qa/qth;
-  var cd=parseFloat(cd).toFixed( 4 );
-  document.getElementById("cd2").innerHTML = cd;
+  var cd=parseFloat(cd).toFixed( 3 );
+  document.getElementById("cd").innerHTML = cd;
   
   var de=4*y2*H;
   var cv= x2/Math.sqrt(de);
-  var cv=parseFloat(cv).toFixed( 4 );
-  document.getElementById("cv2").innerHTML = cv;
+  var cv=parseFloat(cv).toFixed( 3 );
+  //document.getElementById("cv2").innerHTML = cv;
 
   var cc=cd/cv;
-  var cc=parseFloat(cc).toFixed( 4 );
-  document.getElementById("cc2").innerHTML = cc;
+  var cc=parseFloat(cc).toFixed( 3 );
+ // document.getElementById("cc2").innerHTML = cc;
+ document.getElementById("read1").style.visibility = "visible";
+}
 
+else if(slider.value==63.1)
 
-
-
-  document.getElementById("sno3").innerHTML = "4";
-  document.getElementById("height3").innerHTML = "60.815";
-  document.getElementById("x3").innerHTML = "63.1";
-  document.getElementById("y3").innerHTML = "19";
-  document.getElementById("t3").innerHTML = "10";
+  {
+    document.getElementById("sno").innerHTML = "1";
+  document.getElementById("height").innerHTML = "60.815";
+  document.getElementById("x").innerHTML = "63.1";
+  document.getElementById("y").innerHTML = "19";
+  document.getElementById("t").innerHTML = "162";
 
   var x3=63.1;
   var y3=19;
   
-  var t=10;
-  var area=30;
+  var t=162;
+  var area=2500;
   var r=20;
   var qa= (area*r)/t;
-  document.getElementById("qa3").innerHTML = qa;
+  var qa=parseFloat(qa).toFixed( 3 );
+  document.getElementById("qa").innerHTML = qa;
 
   var areaor=1.2667;
   var g=9.81;
   var c= areaor* Math.sqrt(19.62);
   var H=60.815;                          
   var qth= c * Math.sqrt(H); 
-  var qth=parseFloat(qth).toFixed( 4 );
-  document.getElementById("qth3").innerHTML = qth;
+  var qth=parseFloat(qth).toFixed( 3 );
+  document.getElementById("qth").innerHTML = qth;
 
   
   var cd=qa/qth;
-  var cd=parseFloat(cd).toFixed( 4 );
-  document.getElementById("cd3").innerHTML = cd;
+  var cd=parseFloat(cd).toFixed( 3 );
+  document.getElementById("cd").innerHTML = cd;
   
   var de=4*y3*H;
   var cv= x3/Math.sqrt(de);
-  var cv=parseFloat(cv).toFixed( 4 );
-  document.getElementById("cv3").innerHTML = cv;
+  var cv=parseFloat(cv).toFixed( 3 );
+  //document.getElementById("cv3").innerHTML = cv;
 
   var cc=cd/cv;
-  var cc=parseFloat(cc).toFixed( 4 );
-  document.getElementById("cc3").innerHTML = cc;
-
-
+  var cc=parseFloat(cc).toFixed( 3 );
+  //document.getElementById("cc3").innerHTML = cc;
+  document.getElementById("read1").style.visibility = "visible";
+}
+else{
+  document.getElementById("read1").style.visibility = "hidden";
+}
 }
 
 
-
-  var slider1 = document.getElementById("rangey");
-  var output1 = document.getElementById("range-value");
-  output1.innerHTML = slider1.value;
-  
-  slider1.oninput = function() {
-    output1.innerHTML = this.value;
-  }
